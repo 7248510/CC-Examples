2c68ebaf6d9b5e48742f52004803c227d2b3a60d
Caleb <56181475+7248510@users.noreply.github.com>
2021-12-06 01:53:18 -0500

    Create srand.cc

---
+++ srand.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+  
+  std::cout << "Remove the possibility of data loss\n when generating random numbers";
+  srand((unsigned)time(nullptr)); 
+}
